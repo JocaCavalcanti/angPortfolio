8049b7b9f51630acce373d6f26ef159be9d43b30
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2023-12-30 18:42:45 -0300

    :bricks: ci: Finishing the Code Base

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,16 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 //interface
 import { IProjects } from '../../interface/IProjects.interface';
 
+//material
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+
+//enum
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+
+//dialog
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
+
+
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/vfull.png',
@@ -26,5 +38,41 @@ export class ProjectsComponent {
         },
       ],
     },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: '',
+      title: 'Vida FullStack',
+      width: '100px',
+      height: '51px',
+      description: '<p>Esplore o fascinante mundo do desenvolvimento web do Blog dedicado a Front End</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        },
+      ],
+    },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: '',
+      title: 'Vida FullStack',
+      width: '100px',
+      height: '51px',
+      description: '<p>Esplore o fascinante mundo do desenvolvimento web do Blog dedicado a Front End</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        },
+      ],
+    },
+    
   ]);
+
+  public openDialog(data: IProjects){
+    this.#dialog.open(DialogProjectsComponent,{
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    })
+  }
 }

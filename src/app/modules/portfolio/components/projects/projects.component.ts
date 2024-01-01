4a55b7cc9d8554030d47117dc80daa2ecc3e8851
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2024-01-01 16:52:48 -0300

    Fix: Fix projects

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -25,20 +25,6 @@ export class ProjectsComponent {
 
   public arrayProjects = signal<IProjects[]>([
     {
-      src: 'assets/img/projects/vfull.png',
-      alt: '',
-      title: 'Vida FullStack',
-      width: '100px',
-      height: '51px',
-      description: '<p>Esplore o fascinante mundo do desenvolvimento web do Blog dedicado a Front End</p>',
-      links: [
-        {
-          name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br',
-        },
-      ],
-    },
-    {
       src: 'assets/img/projects/AtvM2.png',
       alt: '',
       title: 'Clinica AtivaMente',
